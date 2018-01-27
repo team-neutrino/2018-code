4ee0f4c4afa261a6e58da696a9648cccb478e7a5
NicoleEssner <NicoleEssner@ahs620886ess33834.affiliate.iastate.edu>
2018-01-26 19:39:51 -0600

    Improved constants names

--- 2018-code/src/org/usfirst/frc/team3928/robot/Constants.java
+++ 2018-code/src/org/usfirst/frc/team3928/robot/Constants.java
@@ -11,29 +11,29 @@ public class Constants
 	
 	//Drive
 	public static final int DRIVE_WHEEL_DIAMETER = 4;
-	public static final int ENCODER_COUNTS_PER_ROTATION = 360;
+	public static final int DRIVE_ENCODER_COUNTS_PER_ROTATION = 360;
 	public static final int DRIVE_RIGHT_1_CHANNEL = 1;
 	public static final int DRIVE_RIGHT_2_CHANNEL = 2;
 	public static final int DRIVE_EFT_1_CHANNEL = 3;
 	public static final int DRIVE_LEFT_2_CHANNEL = 4;
 	
-	public static final int RIGHT_ENCODER_POWER_CHANNEL = 7;
-	public static final int RIGHT_ENCODER_DATA_CHANNEL = 8;
-	public static final int LEFT_ENCODER_POWER_CHANNEL = 0;
-	public static final int LEFT_ENCODER_DATA_CHANNEL = 1;
-	public static final double ENCODER_DISTANCE_PER_PULSE = DRIVE_WHEEL_DIAMETER * Math.PI / ENCODER_COUNTS_PER_ROTATION;
+	public static final int DRIVE_RIGHT_ENCODER_POWER_CHANNEL = 7;
+	public static final int DRIVE_RIGHT_ENCODER_DATA_CHANNEL = 8;
+	public static final int DRIVE_LEFT_ENCODER_POWER_CHANNEL = 0;
+	public static final int DRIVE_LEFT_ENCODER_DATA_CHANNEL = 1;
+	public static final double DRIVE_ENCODER_DISTANCE_PER_PULSE = DRIVE_WHEEL_DIAMETER * Math.PI / DRIVE_ENCODER_COUNTS_PER_ROTATION;
 	
-	public static final double NAVX_ANGLE_ADJUSTMENT = 5.5;
+	public static final double DRIVE_NAVX_ANGLE_ADJUSTMENT = 5.5;
 	
-	public static final double P_VALUE_DEGREE_TURN = 0.0275;
-	public static final double I_VALUE_DEGREE_TURN = 0.0;
-	public static final double D_VALUE_DEGREE_TURN = 0.045;
-	public static final int ABSOLUTE_VALUE_TOLERANCE_DEGREE_TURN = 2;
+	public static final double DRIVE_P_VALUE_DEGREE_TURN = 0.0275;
+	public static final double DRIVE_I_VALUE_DEGREE_TURN = 0.0;
+	public static final double DRIVE_D_VALUE_DEGREE_TURN = 0.045;
+	public static final int DRIVE_ABSOLUTE_VALUE_TOLERANCE_DEGREE_TURN = 2;
 	
 	//Elevator
 	public static final int ELEVATOR_ENCODER_POWER_CHANNEL = 4;
 	public static final int ELEVATOR_ENCODER_DATA_CHANNEL = 5;
-	public static final double DISTANCE_TRAVELED_PER_ROTATION = 9.0;
+	public static final double ELEVATOR_DISTANCE_TRAVELED_PER_ROTATION = 9.0;
 	
 	public static final int ELEVATOR_MOTOR_1 = 3;
 	
